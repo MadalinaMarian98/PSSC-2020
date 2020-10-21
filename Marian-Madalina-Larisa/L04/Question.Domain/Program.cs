ccd9705c3941d2e0432df5a43ea1f4a9c139d1a2
Marian Madalina <mady_madalina08@yahoo.com>
2020-10-21 12:46:27 +0300

    Rezolvare tema 4!

---
+++ Marian-Madalina-Larisa/L04/Question.Domain/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Question.Domain.CreateQuestionWorkflow;
+
+namespace Test.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> taglist = new List<string>{ "HTML", "PHP" };
+            var cmd = new CreateQuestionCmd("How to create a website", "How can i create a menu bar with php", taglist);
+            var result = CreateQuestion(cmd);
+
+            result.Match(
+                    ProcessQuestionCreated,
+                    ProcessQuestionNotCreated,
+                    ProcessInvalidQuestion
+                );
+
+            Console.ReadLine();
+        }
+
+        private static string ProcessQuestionNotCreated(CreateQuestionResult.QuestionNotAdded createQuestionResult)
+        {
+            Console.WriteLine("Question was not added. Error: " + createQuestionResult.ErrorMessage.ToString());
+            
+            return createQuestionResult.ErrorMessage.ToString();
+        }
+
+        private static string ProcessInvalidQuestion(CreateQuestionResult.QuestionValidationFailed questionValidation)
+        {
+            Console.WriteLine("failed validation: ");
+            foreach(var error in questionValidation.ValidationErrros)
+            {
+                Console.WriteLine(questionValidation);
+            }
+            return questionValidation.ToString();
+        }
+
+        private static string ProcessQuestionCreated(CreateQuestionResult.QuestionAdded createQuestionResult)
+        {
+            Console.WriteLine("Id:" + createQuestionResult.Id);
+            Console.WriteLine("Title: " + createQuestionResult.Title);
+            Console.WriteLine("Body: " + createQuestionResult.Text);
+            Console.WriteLine("Tags:");
+            foreach(var item in createQuestionResult.Tags)
+            {
+                Console.WriteLine(item);
+            }
+
+            return createQuestionResult.ToString();
+        }
+
+        static CreateQuestionResult.IAskQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if(string.IsNullOrWhiteSpace(createQuestionCommand.Title))
+            {
+                string error = new string ("Title is empty!But it hasn't had to be");
+                return new CreateQuestionResult.QuestionNotAdded(error);
+            }
+
+            if(string.IsNullOrWhiteSpace(createQuestionCommand.Text))
+            {
+                string error = new string ("Body is empty!But it hasn't had to be");
+                return new CreateQuestionResult.QuestionNotAdded(error);
+            }
+
+             if(new Random().Next(10) > 7) 
+            {
+                var error = new List<string> {"Validation Failed"};
+                return new CreateQuestionResult.QuestionValidationFailed(error);
+            }
+
+
+            var Id = Guid.NewGuid();
+            var result = new CreateQuestionResult.QuestionAdded(Id, createQuestionCommand.Title, createQuestionCommand.Text, createQuestionCommand.Tags);
+            return result;
+        }
+    }
+}
---
+++ Marian-Madalina-Larisa/L04/Test.App/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Question.Domain.AskQuestionWorkflow;
+
+namespace Test.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> taglist = new List<string>{ "HTML", "PHP" };
+            var cmd = new CreateQuestionCmd("How to create a submeniu", "How can i stylized a drop-down menu with css  ", taglist);
+            var result = CreateQuestionCmd(cmd);
+            
+            result.Match(
+                ProcessQuestionAdded,
+                ProcessQuestionNotAdded,
+                ProcessQuestionValidationFailed
+            );
+        }
+
+        private static string ProcessQuestionNotAdded(CreateQuestionResult.QuestionNotAdded question)
+        {
+            Console.WriteLine("Question was not added. Error: " + question.ErrorMessage.ToString());
+            
+            return question.ErrorMessage.ToString();
+        }
+
+        private static string ProcessQuestionValidationFailed(CreateQuestionResult.QuestionValidationFailed question)
+        {
+            Console.WriteLine("Failed validation: ");
+            foreach(var error in question.ValidationErrros)
+            {
+                Console.WriteLine(question);
+            }
+
+        }
+
+        private static string ProcessQuestionAdded(CreateQuestionResult.QuestionAdded question)
+        {
+            Console.WriteLine("Id:" + question.Id);
+            Console.WriteLine("Title: " + question.Title);
+            Console.WriteLine("Body: " + question.Text);
+            Console.WriteLine("Tags:");
+            foreach(var item in question.Tags)
+            {
+                Console.WriteLine(item);
+            }
+
+            return question.ToString();
+        }
+
+        static CreateQuestionResult.IAskQuestionResult AskQuestion(CreateQuestionCmd cmd)
+        {
+            if(string.IsNullOrWhiteSpace(cmd.Title))
+            {
+                string error = new string ("Title is empty! But it hasn't had to be...");
+                return new CreateQuestionResult.QuestionNotAdded(error);
+            }
+
+            if(string.IsNullOrWhiteSpace(cmd.Text))
+            {
+                string error = new string ("Body is empty but it hasn't had to be...!");
+                return new CreateQuestionResult.QuestionNotAdded(error);
+            }
+
+            if(new Random().Next(10) > 7) //simulare analiza text
+            {
+                var error = new List<string> {" validation failed!"};
+                return new CreateQuestionResult.QuestionValidationFailed(error);
+            }
+
+            var Id = Guid.NewGuid();
+            var result = new CreateQuestionResult.QuestionAdded(Id, cmd.Title, cmd.Text, cmd.Tags);
+            return result;
+        }
+    }
+}
